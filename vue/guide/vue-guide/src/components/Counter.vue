a214b1be55d6bba83ec9041934d843b3f8b9c2c9
lange <lange@synyx.de>
2024-05-26 09:52:26 +0200

    extending counter button

--- vue/guide/vue-guide/src/components/Counter.vue
+++ vue/guide/vue-guide/src/components/Counter.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
-import {ref} from 'vue';
+import {onMounted, ref} from 'vue';
 const count = ref(0);
+
+function increment() {
+  count.value++
+}
+
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`);
+})
 </script>
 
 <template>
-  <button @click="count++">Count is: {{ count }}</button>
+  <button @click="increment">Count is: {{ count }}</button>
 </template>
 
 <style scoped>
